033c128ea8b4dc0e1a5f98cb1c92fab104f89b19
approvedm <126514054+approvedm@users.noreply.github.com>
2024-03-10 11:46:39 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://jackfortyn:abw5tQmA7cJNQDQp@cluster0.qsxvz2l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',//database link
+    
+    bot_token:'6923052207:AAEHfqb02c53LnBG-Lt3gUjGLaItAwspcUw',//bot token
+    
+    bot_name:'autpaybbot',//bot username without @
+    
+    bot_admin:6954021372,// admin telegram id
+    
+    channelsList : ['@autpay'],// channels list
+    
+    reffer_bonus:5,// refferal bonus amount
+    
+    min_wd:0.00000001,//min withdrawal amount
+    
+    daily_bonus:100,// daily bonus amount
+    
+    currency:'MATIC', //bot currency
+    
+    contractaddress :  '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0', //contract address of coin
+    
+    privateKey : 'c01e724dfadad4fe38b6229c7e98d62d14403396b17121a8d0b31676067ea988', //private key of wallet
+    
+    address : '0x042048aE0702f87BC1901E7D30b9D479594712a6',//polygon address of wallet
+    
+    payment_channel:'@autpay'// payment channel username
+    
+    }
